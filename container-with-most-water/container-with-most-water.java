da5b20dd73855c6136cc8fffa08e752ee5deb575
Aniket Dash <dash.aniket05@gmail.com>
2021-05-01 00:16:59 -0400

    Time: 3 ms (77.54%), Space: 52.9 MB (18.95%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=Integer.MIN_VALUE;
+        int i=0,j=height.length-1;
+        while(i<=j){
+            int area=Math.min(height[i],height[j])*(j-i);
+            max=Math.max(area,max);
+            if(height[i]<=height[j])
+                i++;
+            else
+                j--;
+        }
+        return max;
+    }
+}
