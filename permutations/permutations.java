c73b902d8cb91d5e433db86f2338ed3762a40c9b
Aniket Dash <dash.aniket05@gmail.com>
2021-05-16 20:10:24 -0400

    Time: 3 ms (15.20%), Space: 38.9 MB (84.31%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,23 +1,25 @@
 class Solution {
+    
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        LinkedList<Integer> perm=new LinkedList<>();
-        Set<Integer> lookup=new HashSet<>();
-        backtrack(nums,perm,result,lookup);
-        return result;
+        List<List<Integer>> res=new ArrayList();
+        LinkedList<Integer> li= new LinkedList();
+        Set<Integer> st=new HashSet();
+        backtrack(res,li,st,nums);
+        return res;
     }
-    public void backtrack(int[] nums,LinkedList<Integer> perm,List<List<Integer>> result,Set<Integer> lookup){
-        if(perm.size()==nums.length){
-            result.add(new ArrayList<>(perm));
+    
+    public void backtrack(List<List<Integer>> res,LinkedList<Integer> li,Set<Integer> st,int[] nums){
+        if(li.size()==nums.length){
+            res.add(new ArrayList<>(li));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(lookup.contains(nums[i])) continue;
-            perm.add(nums[i]);
-            lookup.add(nums[i]);
-            backtrack(nums,perm,result,lookup);
-            lookup.remove(nums[i]);
-            perm.removeLast();
+            if(st.contains(nums[i])) continue;
+            li.add(nums[i]);
+            st.add(nums[i]);
+            backtrack(res,li,st,nums);
+            li.removeLast();
+            st.remove(nums[i]);
         }
     }
 }
